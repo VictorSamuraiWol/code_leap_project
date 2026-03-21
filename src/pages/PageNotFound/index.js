c1a7134c04e53b80a35970d53a53649ec1ac420e
Victor Cardoso <victorclmatos@gmail.com>
2026-03-21 18:10:07 -0300

    adjust PageNotFound

--- src/pages/PageNotFound/index.js
+++ src/pages/PageNotFound/index.js
@@ -1,13 +1,28 @@
 import style from './style.css';
+import { Link } from 'react-router-dom';
 
 function PageNotFound() {
-  
-  return (
-    <div>
-      PageNotFound
-    </div>
-  )
+    return(
+        <div className='page-not-found'>
+            <div>
+                <span className='text404'>404</span>
 
+                <h1 className='title'>
+                    Oops! Page not found!
+                </h1>
+                
+            </div>
+
+            <p className='paragrafh'>
+                Is this what you were looking for? Please verify the address in the adress bar or click the link below to return to the previous page.
+            </p>
+
+            <Link to='/'>
+                <div className='back-page'>Previous Page</div>
+            </Link>
+
+        </div>
+    )
 }
 
 export default PageNotFound;
